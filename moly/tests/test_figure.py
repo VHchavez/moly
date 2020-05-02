b332b2e703596e5914158d745761f359964b9fdf
Victor H. Chavez <gonza445@purdue.edu>
2020-05-01 20:00:32 -0400

    testfiles now on tests folder

--- moly/tests/test_figure.py
+++ moly/tests/test_figure.py
@@ -37,18 +37,18 @@ def test_add_molecule_from_data_tubes(he_dimer):
 
 def test_add_molecule_from_file():
     fig = moly.Figure()
-    mol = moly.Molecule.from_file("test_files/water.xyz")
+    mol = moly.Molecule.from_file("water.xyz")
 
 def test_figsize():
     fig = moly.Figure(figsize=(300,300))
 
 def test_add_cube():
     fig = moly.Figure()
-    fig.add_cube("test_files/Da.cube", iso=0.03, colorscale="rdbu", opacity=0.2)
+    fig.add_cube("Da.cube", iso=0.03, colorscale="rdbu", opacity=0.2)
 
 def test_add_cube_slider():
     fig = moly.Figure()
-    fig.add_cube("test_files/Da.cube", iso=[0.01, 0.1], colorscale="rdbu", opacity=0.2)
+    fig.add_cube("Da.cube", iso=[0.01, 0.1], colorscale="rdbu", opacity=0.2)
 
 def test_add_cubes():
     fig = moly.Figure()
